94765767a2ae05489a449e1fbd117a1cedf98292
RenMarTom <105501877+RenMarTom@users.noreply.github.com>
2023-05-04 14:15:51 +0700

    Create вратарь_новый.cpp

---
+++ вратарь_новый.cpp
@@ -0,0 +1,71 @@
+     #define IRSEEKER IN_1
+
+// Convenience macro that combines TextOut and NumOut
+#define TextNumOut(xPos,yPos,str,col,num)  TextOut(xPos,yPos,str); \
+                                           NumOut(xPos+6*col,yPos,num)
+task main()
+{
+int d;
+long t0, time;
+int heading1, azimut;
+SetSensorLowspeed(IN_4);
+SetSensorColorRed(S3);
+SetSensorColorRed(S2);
+SetSensorLowspeed(IN_1);
+  int dir, s1, s2, s3, s4, s5, result;
+    heading1 = SensorHTCompass(IN_4);
+    azimut = heading1;
+  SetSensorLowspeed(IRSEEKER);
+    int UP;
+
+
+
+  while(1)
+{
+
+
+    ReadSensorHTIRSeeker2AC(IRSEEKER, dir, s1, s2, s3, s4, s5);
+    heading1 = SensorHTCompass(IN_4); // get current heading
+    UP = (heading1 + 540 - azimut) % 360 - 180;
+    TextNumOut(0, LCD_LINE3, "Dir:    ",4,dir);
+    TextNumOut(6, LCD_LINE4, "S1:     ",3,s1);
+    TextNumOut(6, LCD_LINE5, "S2:     ",3,s2);
+    TextNumOut(6, LCD_LINE6, "S3:     ",3,s3);
+    TextNumOut(6, LCD_LINE7, "S4:     ",3,s4);
+    TextNumOut(6, LCD_LINE8, "S5:     ",3,s5);
+    TextNumOut(6, LCD_LINE2, "UP:     ",4,UP);
+
+
+   Wait(1);
+    d = s1 + s2 + s3 + s4 + s5;
+
+
+
+
+    if(dir > 6)
+   {
+
+   OnFwd(OUT_BC, -90);
+      Wait(100);
+      }
+   if(dir < 6)
+   {
+   OnFwd(OUT_BC, 90);
+      Wait(100);
+   }
+    if(dir == 6)
+    {
+       OnFwd(OUT_BC, 0);
+          Wait(100);
+   }
+  }
+
+
+
+
+
+
+
+
+
+}
