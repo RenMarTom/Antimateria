16cd8b85155af35f5dcbf614f2a0689a5c9c78e3
RenMarTom <105501877+RenMarTom@users.noreply.github.com>
2023-05-11 22:46:39 +0700

    Create POPRYGUNCHIK.ino

---
+++ POPRYGUNCHIK.ino
@@ -0,0 +1,93 @@
+ #define IRSEEKER IN_1
+
+// Convenience macro that combines TextOut and NumOut
+#define TextNumOut(xPos,yPos,str,col,num)  TextOut(xPos,yPos,str); \
+                                           NumOut(xPos+6*col,yPos,num)
+task main()
+{
+//int d;
+
+int heading1, azimut;
+SetSensorLowspeed(IN_4);
+SetSensorColorRed(S3);
+SetSensorColorRed(S2);
+SetSensorLowspeed(IN_1);
+  int dir, s1, s2, s3, s4, s5, result;
+    heading1 = SensorHTCompass(IN_4);
+    azimut = heading1;
+ SetSensorLowspeed(IRSEEKER);
+    int UP;
+    int Us;
+    int red;
+    int zeP;
+    int zeZ;
+    zeZ = SENSOR_2;
+    zeP = SENSOR_3;
+
+  while(1)
+{
+  Us=SENSOR_2;
+red=SENSOR_3;
+
+   if(Us > zeZ+2)
+   { OnFwd(OUT_BC, 90);
+   Wait(1000);}
+    if(red > zeP+2)
+   { OnFwd(OUT_BC, -90);
+   Wait(1000);}
+
+if(UP > 3)
+   {
+     OnFwd(OUT_B, 35);
+     OnFwd(OUT_C, -35);
+
+      }
+
+   if(UP < -3)
+   {
+     OnFwd(OUT_B, -35);
+     OnFwd(OUT_C, 35);
+       }
+
+    ReadSensorHTIRSeeker2AC(IRSEEKER, dir, s1, s2, s3, s4, s5);
+    heading1 = SensorHTCompass(IN_4); // get current heading
+    UP = (heading1 + 540 - azimut) % 360 - 180;
+    TextNumOut(0, LCD_LINE3, "Dir:    ",4,dir);
+    TextNumOut(6, LCD_LINE4, "S1:     ",3,s1);
+    TextNumOut(6, LCD_LINE5, "S2:     ",3,s2);
+    TextNumOut(6, LCD_LINE6, "S3:     ",3,s3);
+    TextNumOut(6, LCD_LINE7, "red:     ",3,red);
+    TextNumOut(6, LCD_LINE8, "Us:     ",3,Us);
+    TextNumOut(6, LCD_LINE2, "UP:     ",4,UP);
+
+
+  Wait(1);
+
+  // d = s1 + s2 + s3 + s4 + s5;
+
+
+   if(dir > 6)
+   {
+  OnFwd(OUT_BC, -90);
+
+      }
+
+
+
+
+
+   if(dir < 6)
+   {
+   OnFwd(OUT_BC, 90);
+
+
+
+
+      }
+    if(dir == 6)
+    {
+       OnFwd(OUT_BC, 0);
+   }
+
+  }
+ }
